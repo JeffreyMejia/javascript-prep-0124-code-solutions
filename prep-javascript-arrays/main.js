4485606dc52b9843faa926eba0371c69d179ffcb
Jeffrey Mejia Cortez <jeffreym951@gmail.com>
2024-01-12 04:48:02 +0000

    completed prep-javascript-arrays exercise

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+const languages = ["html", "css", "javascript", "react", "node", "postgres"];
+console.log ("languages array", languages);
+
+const firstelement = languages[0];
+console.log ("the first element of the array is", firstelement);
+
+const thirdelement = languages[2];
+console.log ("the 3rd element of the array is", thirdelement);
+
+const length = languages.length;
+console.log ("the length of the array is", length);
+
+const lastelement = languages[languages.length - 1]
+console.log ("the last element of the array is", lastelement);
